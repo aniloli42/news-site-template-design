d114d909045b8a173c1f1767aabfb7cfa46653ad
aniloli42 <aniloli42@gmail.com>
2021-06-06 17:58:37 +0545

    navbar animated

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@ function getData() {
 }
 
 // getData();
+
+const menuBar = document.getElementById("menuBar");
+menuBar.addEventListener("click", (e) => {
+  const navBar = document.getElementById("navBar");
+  menuBar.classList.toggle("open");
+  if (menuBar.classList.contains("open")) {
+    navBar.style.cssText = "transform:scaleY(1);";
+    navBar.style.maxHeight = "200px";
+  }
+  if (!menuBar.classList.contains("open")) {
+    navBar.style.cssText = "transform:scaleY(0);";
+    navBar.style.maxHeight = "0px";
+  }
+});
