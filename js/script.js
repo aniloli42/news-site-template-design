f1381370ca54accfc51b82532931f39692b0d466
aniloli42 <aniloli42@gmail.com>
2021-07-07 18:26:23 +0545

    change content cloning function added and style fix

--- js/script.js
+++ js/script.js
@@ -22,3 +22,27 @@ menuBar.addEventListener("click", (e) => {
     });
   });
 });
+
+// variables for clone the main content
+const mainContent = document.querySelector(".main-content");
+const mainDiv = document.querySelector(".main-div");
+
+cloneContentMaker(mainContent, mainDiv, 2);
+
+// variables for clone the popular content
+const popularContent = document.querySelector(".popular-content");
+const popularDiv = document.querySelector(".popular-body");
+
+cloneContentMaker(popularContent, popularDiv, 2);
+
+// implement the function which clone the content and add on particular div
+function cloneContentMaker(source, target, loopTimes) {
+  let intialLoop = 0;
+  while (intialLoop < loopTimes) {
+    let cloneContent = source.cloneNode(true);
+
+    // appending clone content
+    target.appendChild(cloneContent);
+    intialLoop++;
+  }
+}
